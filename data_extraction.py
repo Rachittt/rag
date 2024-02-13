2521d39796183ccddecfb057212fb1592ca0c13a
Rachittt <chourasiarachit@gmail.com>
2024-02-13 21:39:35 +0530

    added requirements.txt and data_extraction.py

---
+++ data_extraction.py
@@ -0,0 +1,64 @@
+from duckduckgo_search import DDGS
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+from tqdm import tqdm
+import pandas as pd
+
+
+def link_extractor():
+    with DDGS(timeout=60) as ddgs:
+        results = [r for r in ddgs.text("Canoo", max_results=1000)]
+        print("Link Extraction Done")
+
+    urls = [url['href'] for url in results]
+    return urls
+
+
+def data_extractor(urls):
+    all_data = {}
+
+    options = webdriver.ChromeOptions()
+    options.add_argument("--test-type")
+    options.add_argument("--disable-default-apps")
+    options.add_argument("--ignore-certificate-errors");
+    options.add_argument("--incognito");
+    options.add_argument("--page-load-strategy='eager'")
+    options.add_argument("--unhandled-prompt-behavior='dismiss'")
+
+    for url in tqdm(urls):
+        try:
+            driver = webdriver.Chrome(options=options)
+            driver.get(url)
+        except Exception as e:
+            print(e)
+        time.sleep(3)
+        try:
+            data = driver.find_element(By.TAG_NAME, "body").text
+        except Exception as e:
+            print(e)
+        all_data[url] = data
+        driver.quit()
+
+    return all_data
+
+
+def replace(x):
+    return x.replace("\n", " ")
+
+def create_preprocess_save_dataframe(all_data):
+    df = pd.DataFrame(list(all_data.items()), index=None, columns=['url', 'data'])
+
+    df['data'] = df['data'].apply(replace)
+
+    df = df[~df['data'].str.contains('404 This')]
+
+    df = df[df['data']!='']
+
+    df.to_csv('data.csv', index=None)
+
+
+if __name__=='__main__':
+    urls = link_extractor()
+    all_data = data_extractor(urls)
+    create_preprocess_save_dataframe(all_data)
